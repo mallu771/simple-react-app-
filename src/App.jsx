1a30dd9e4fec4e9555a9042d726378e1741b5b51
mallu771 <mallikarjunbajantri891@gmail.com>
2025-07-19 17:43:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+function App() {
+  return (
+    <div>
+      <h1>React Docker + Vercel Demo</h1>
+      <p>Hello from containerized React!</p>
+    </div>
+  );
+}
+
+export default App;
+
